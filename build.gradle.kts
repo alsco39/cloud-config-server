a8758afee1016b77fb8898bcd7618f312f076fc1
min56 <minchaeg85@gmail.com>
2023-04-11 08:19:26 +0900

    ⚡️ :: security 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.cloud:spring-cloud-config-server")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 }
 
 dependencyManagement {
