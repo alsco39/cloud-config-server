87b1ac499c3fec235f6da891c7bfff3d966b1215
min56 <minchaeg85@gmail.com>
2023-04-15 21:12:59 +0900

    ⚡️ :: @EnableWebSecurity 추가

--- src/main/kotlin/com/example/cloudconfigserver/CloudConfigServerApplication.kt
+++ src/main/kotlin/com/example/cloudconfigserver/CloudConfigServerApplication.kt
@@ -3,7 +3,9 @@ package com.example.cloudconfigserver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
+@EnableWebSecurity
 @EnableConfigServer
 @SpringBootApplication
 class CloudConfigServerApplication
